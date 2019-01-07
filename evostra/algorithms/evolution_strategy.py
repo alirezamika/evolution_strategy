95530c4dc1da202a5727e88b2099613889a1f091
KOLANICH <kolan_n@mail.ru>
2019-01-07 17:16:37 +0300

    Added distributions, adam and tests (surprisingly adam works worse than simple gradient descent)

--- evostra/algorithms/evolution_strategy.py
+++ evostra/algorithms/evolution_strategy.py
@@ -1,6 +1,8 @@
 from __future__ import print_function
 import numpy as np
+import scipy.stats as st
 import multiprocessing as mp
+from collections.abc import Iterable
 
 np.random.seed(0)
 
@@ -9,24 +11,66 @@ def worker_process(arg):
     get_reward_func, weights = arg
     return get_reward_func(weights)
 
+class WeightUpdateStrategy:
+    __slots__ = ("learning_rate",)
+    def __init__(self, dim, learning_rate):
+        self.learning_rate = learning_rate
+
+
+class strategies:
+    class GD(WeightUpdateStrategy):
+        def update(self, i, g):
+            return self.learning_rate * g
+
+
+    class Adam(WeightUpdateStrategy):
+        __slots__ = ("eps", "beta1", "beta2", "m", "v")
+        def __init__(self, dim, learning_rate, eps=1e-8, beta1=0.9, beta2=0.999):
+            super().__init__(dim, learning_rate)
+            self.eps = eps
+            self.beta1 = beta1
+            self.beta2 = beta2
+            self.m = np.zeros(dim)
+            self.v = np.zeros(dim)
+
+        def update(self, i, g):
+            self.m[i] = self.beta1 * self.m[i] + (1-self.beta1) * g
+            self.v[i] = self.beta2 * self.v[i] + (1-self.beta2) * (g**2)
+            return self.learning_rate * np.sqrt(1-self.beta2) / (1-self.beta1) * self.m[i] / np.sqrt(np.sqrt(self.v[i])+self.eps)
+
 
 class EvolutionStrategy(object):
     def __init__(self, weights, get_reward_func, population_size=50, sigma=0.1, learning_rate=0.03, decay=0.999,
-                 num_threads=1):
-
+                 num_threads=1, limits=None, printer=None, distributions=None, strategy=None):
+        if limits is None:
+            limits = (np.inf, -np.inf)
         self.weights = weights
+        self.limits = limits
         self.get_reward = get_reward_func
         self.POPULATION_SIZE = population_size
-        self.SIGMA = sigma
+        if distributions is None:
+            distributions = st.norm(loc=0., scale=sigma)
+        if isinstance(distributions, Iterable):
+            distributions = list(distributions)
+            self.SIGMA = np.array([d.std() for d in distributions])
+        else:
+            self.SIGMA = distributions.std()
+
+        self.distributions = distributions
         self.learning_rate = learning_rate
         self.decay = decay
         self.num_threads = mp.cpu_count() if num_threads == -1 else num_threads
+        if printer is None:
+            printer = print
+        self.printer = printer
+        if strategy is None:
+            strategy = strategies.GD
+        self.strategy = strategy(len(weights), self.learning_rate)
 
     def _get_weights_try(self, w, p):
         weights_try = []
         for index, i in enumerate(p):
-            jittered = self.SIGMA * i
-            weights_try.append(w[index] + jittered)
+            weights_try.append(w[index] + i)
         return weights_try
 
     def get_weights(self):
@@ -36,8 +80,13 @@ class EvolutionStrategy(object):
         population = []
         for i in range(self.POPULATION_SIZE):
             x = []
-            for w in self.weights:
-                x.append(np.random.randn(*w.shape))
+            if isinstance(self.distributions, Iterable):
+                for j, w in enumerate(self.weights):
+                    x.append(self.distributions[j].rvs(*w.shape))
+            else:
+                for w in self.weights:
+                    x.append(self.distributions.rvs(*w.shape))
+
             population.append(x)
         return population
 
@@ -59,10 +108,17 @@ class EvolutionStrategy(object):
         if std == 0:
             return
         rewards = (rewards - rewards.mean()) / std
+        grad_factor = 1. / (self.POPULATION_SIZE * (self.SIGMA ** 2))
+
         for index, w in enumerate(self.weights):
             layer_population = np.array([p[index] for p in population])
-            update_factor = self.learning_rate / (self.POPULATION_SIZE * self.SIGMA)
-            self.weights[index] = w + update_factor * np.dot(layer_population.T, rewards).T
+            corr = np.dot(layer_population.T, rewards).T
+
+            if not isinstance(grad_factor, np.ndarray):
+                g = grad_factor * corr
+            else:
+                g = grad_factor[index] * corr
+            self.weights[index] = w + self.strategy.update(index, g)
         self.learning_rate *= self.decay
 
     def run(self, iterations, print_step=10):
@@ -75,7 +131,7 @@ class EvolutionStrategy(object):
             self._update_weights(rewards, population)
 
             if (iteration + 1) % print_step == 0:
-                print('iter %d. reward: %f' % (iteration + 1, self.get_reward(self.weights)))
+                self.printer('iter %d. reward: %f' % (iteration + 1, self.get_reward(self.weights)), (self.weights if self.weights.shape[0] <= 10 else None) )
         if pool is not None:
             pool.close()
             pool.join()
